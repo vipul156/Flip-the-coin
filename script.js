4167f1745ba5a949a2ec65b3336c1d5697c99e03
vipul156 <38092536+vipul156@users.noreply.github.com>
2025-10-20 12:22:36 +0530

    Implement coin flip feature with animation
    Updated coin flip logic to include rotation effect and result display.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const button = document.querySelector("button")
-const img = document.querySelector("img")
-const changeImg= ()=>{
-  const random = Math.random()
-  img.src= random > 0.5 ? "/resources/tails.svg" : "/resources/heads.svg";
-}
-button.addEventListener('click',changeImg)
-img.addEventListener('click',changeImg)
+let flipBtn = document.querySelector("button");
+let coin = document.querySelector("img");
+let result = document.querySelector(".resultP");
+
+// make random number of fliping coin (if number even => head , odd=> tail)
+flipBtn.addEventListener("click" , ()=> {
+
+    let theRandomNum = Math.trunc(Math.random()*20);
+    coin.style.transform = `rotateY(${-180*theRandomNum}deg)`;
+    setTimeout(() => {
+        if(theRandomNum%2 == 0) {
+            result.textContent = "Heads"
+        } else {
+            result.textContent = "Tails";
+    }
+    }, 1000);
+    
+
+});
